4bfaa0002ffec04a84306cc5e35cf4e961f2b295
Eric Yan <xyan@humana.com>
2019-08-15 15:51:29 +0800

    replace count with event

--- src/main/scala/com/machinedoll/gate/generator/ResettableCustomSource.scala
+++ src/main/scala/com/machinedoll/gate/generator/ResettableCustomSource.scala
@@ -3,7 +3,7 @@ package com.machinedoll.gate.generator
 import java.util.Calendar
 
 import com.machinedoll.gate.schema.{DescriptionExample, EventTest}
-import org.apache.flink.api.common.state.ListState
+import org.apache.flink.api.common.state.{ListState, ListStateDescriptor}
 import org.apache.flink.runtime.state.{FunctionInitializationContext, FunctionSnapshotContext}
 import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction
 import org.apache.flink.streaming.api.functions.source.SourceFunction
@@ -11,45 +11,18 @@ import org.apache.flink.streaming.api.functions.source.SourceFunction
 class ResettableCustomSource extends SourceFunction[EventTest] with CheckpointedFunction{
   var isRunning: Boolean = true
   var cnt: Long = _
+  var event: EventTest = _
   var offsetState: ListState[EventTest] = _
   val calenderInstance = Calendar.getInstance()
 
-
-
-//  override def run(ctx: SourceFunction.SourceContext[Long]): Unit = {
-//    while (isRunning && cnt < Long.MaxValue) {
-//      ctx.getCheckpointLock.synchronized {
-//        cnt += 1
-//        ctx.collect(cnt)
-//      }
-//    }
-//  }
-
-//  override def cancel(): Unit = isRunning = false
-//
-//  override def snapshotState(context: FunctionSnapshotContext): Unit = {
-//    offsetState.clear()
-//    offsetState.add(cnt)
-//  }
-//
-//  override def initializeState(context: FunctionInitializationContext): Unit = {
-//    val desc = new ListStateDescriptor[Long]("offset", classOf[Long])
-//    offsetState = context.getOperatorStateStore.getListState(desc)
-//
-//    val it = offsetState.get()
-//    cnt = if (null == it || !it.iterator().hasNext) {
-//      -1L
-//    } else {
-//      it.iterator().next()
-//    }
-//  }
   override def run(ctx: SourceFunction.SourceContext[EventTest]): Unit =
     while (isRunning && cnt < Long.MaxValue) {
       ctx.getCheckpointLock.synchronized{
         cnt += 1
-        ctx.collect(new EventTest(id = cnt.toInt,
+        event = new EventTest(id = cnt.toInt,
           timestamp = calenderInstance.getTimeInMillis,
-          description = new DescriptionExample(0, "0")) )
+          description = new DescriptionExample(0, "0"))
+        ctx.collect(event)
       }
     }
 
@@ -57,11 +30,20 @@ class ResettableCustomSource extends SourceFunction[EventTest] with Checkpointed
 
   override def snapshotState(context: FunctionSnapshotContext): Unit = {
     offsetState.clear()
-    offsetState.add(
-      new EventTest(cnt.toInt,
-        context.getCheckpointTimestamp,
-        description = new DescriptionExample(0, "0")))
+    offsetState.add(event)
   }
 
-  override def initializeState(context: FunctionInitializationContext): Unit = ???
+  override def initializeState(context: FunctionInitializationContext): Unit = {
+    val desc = new ListStateDescriptor[EventTest]("offset", classOf[EventTest])
+    offsetState = context.getOperatorStateStore.getListState(desc)
+
+    val it = offsetState.get()
+    event = if (null == it || !it.iterator().hasNext) {
+      new EventTest(-1,
+        calenderInstance.getTimeInMillis,
+        new DescriptionExample(-1, "-1"))
+    } else {
+      it.iterator().next()
+    }
+  }
 }
