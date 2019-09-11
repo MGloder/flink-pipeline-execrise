fe69ed993efb96b03a7a1e1474d041d9b9cb1a0c
Eric Yan <xyan@humana.com>
2019-09-11 09:03:57 +0800

    added a custom serializer for kafka

--- src/main/scala/com/machinedoll/gate/sink/SinkCollection.scala
+++ src/main/scala/com/machinedoll/gate/sink/SinkCollection.scala
@@ -1,14 +1,19 @@
 package com.machinedoll.gate.sink
 
+import java.io.{ByteArrayOutputStream, ObjectOutputStream}
+import java.lang
 import java.util.{Optional, Properties}
 
 import com.machinedoll.gate.partitioner.KafkaCustomPartitioner
+import com.machinedoll.gate.schema.EventTest
 import com.typesafe.config.Config
 import org.apache.flink.api.common.serialization.SimpleStringSchema
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer
+import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaProducer, KafkaSerializationSchema}
 import org.apache.flink.streaming.connectors.kafka.partitioner.FlinkKafkaPartitioner
+import org.apache.kafka.clients.producer.ProducerRecord
 
 object SinkCollection {
+
   def getKafkaStringSinkTest(config: Config, topic: String): FlinkKafkaProducer[String] = {
 
     val props = new Properties()
@@ -43,5 +48,25 @@ object SinkCollection {
     new FlinkKafkaProducer(topic, new SimpleStringSchema(), props, customKafkaPartitioner)
   }
 
+  def getKafkaCustomSerializerSink(config: Config, topic: String) = {
+    val props = new Properties()
+    props.setProperty("bootstrap.servers",
+      config.getString("kafka.kafka-server"))
+    props.setProperty("zookeeper.connect",
+      config.getString("kafka.zookeeper-server"))
+    props.setProperty("group.id",
+      config.getString("kafka.group.id"))
 
+    new FlinkKafkaProducer[EventTest](topic, new KafkaSerializationSchema[EventTest] {
+      val stream: ByteArrayOutputStream = new ByteArrayOutputStream()
+      val oos = new ObjectOutputStream(stream)
+
+      override def serialize(element: EventTest, timestamp: lang.Long): ProducerRecord[Array[Byte], Array[Byte]] ={
+        oos.writeObject(element)
+        oos.close()
+        val result = stream.toByteArray
+        new ProducerRecord[Array[Byte], Array[Byte]](topic, result)
+      }
+    }, props, FlinkKafkaProducer.Semantic.EXACTLY_ONCE)
+  }
 }
